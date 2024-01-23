7b5d6196ea3eece88d37922d521f4d5060175e77
kelter-js <spawnkillerqw@gmail.com>
2024-01-23 23:02:49 +0500

    feat: Feed component

---
+++ components/Feed/Feed.tsx
@@ -0,0 +1,11 @@
+import Section from "../Section";
+
+const Feed = () => (
+  <>
+    <Section title="Science" />
+    <Section title="Technology" />
+    <Section title="Arts" />
+  </>
+);
+
+export default Feed;
