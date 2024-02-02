217229a7019f82d5343812ee07afb41c21bd3581
kelter-js <spawnkillerqw@gmail.com>
2024-02-02 19:01:54 +0500

    fix: typization errors

--- components/Post/PostCard.tsx
+++ components/Post/PostCard.tsx
@@ -1,25 +1,22 @@
 import { FC } from "react";
-import Link from "next/link";
 
 import { Card, Figure, Title, Excerpt } from "./PostCardStyle";
 
 const PostCard: FC = () => (
-  <Link href="/post/example" passHref>
-    <Card>
-      <Figure>
-        <img alt="Post photo" src="/image1.jpg" />
-      </Figure>
+  <Card href="/post/example">
+    <Figure>
+      <img alt="Post photo" src="/image1.jpg" />
+    </Figure>
 
-      <Title>Post title!</Title>
+    <Title>Post title!</Title>
 
-      <Excerpt>
-        <p>
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-          eiusmod tempor incididunt ut labore et dolore magna aliqua.
-        </p>
-      </Excerpt>
-    </Card>
-  </Link>
+    <Excerpt>
+      <p>
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
+        tempor incididunt ut labore et dolore magna aliqua.
+      </p>
+    </Excerpt>
+  </Card>
 );
 
 export default PostCard;
