6fbdb1239f02941b23c826139902689b989d0172
kelter-js <spawnkillerqw@gmail.com>
2024-02-02 19:03:07 +0500

    feat: updated imports

--- MyApp.tsx
+++ MyApp.tsx
@@ -2,8 +2,8 @@ import React from "react";
 import Head from "next/head";
 import { ThemeProvider } from "styled-components";
 
-import { Header } from "./components/Header";
-import { Footer } from "./components/Footer";
+import Header from "./components/Header";
+import Footer from "./components/Footer";
 import { Center } from "./components/Center";
 import { GlobalStyle, theme } from "./shared/theme";
 
