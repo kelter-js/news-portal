1f74711cc75dea2104f5c694ed9aaf03cce7db0e
kelter-js <spawnkillerqw@gmail.com>
2024-05-14 23:00:26 +0500

    feat: another url base test

--- api/request/config.ts
+++ api/request/config.ts
@@ -1,8 +1,8 @@
 const IS_PRODUCTION = process.env.NODE_ENV === "production";
 
 const protocol = IS_PRODUCTION ? "https" : "http";
-const host = process.env.NEXT_PUBLIC_BASE_URL || "localhost:3000";
+const apiUrl = process.env.NEXT_PUBLIC_API_URL || "localhost:3000";
 
 export const config = {
-  baseUrl: `https://news-portal-o92egsazf-arkadiys-projects-29e903eb.vercel.app/api`,
+  baseUrl: `${apiUrl}/api`,
 };
