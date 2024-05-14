d981daf52fadc9f11d95c62485d3aecaee6b1215
kelter-js <spawnkillerqw@gmail.com>
2024-05-14 22:42:34 +0500

    feat: added new deploy settings

--- api/request/config.ts
+++ api/request/config.ts
@@ -1,8 +1,8 @@
 const IS_PRODUCTION = process.env.NODE_ENV === "production";
 
 const protocol = IS_PRODUCTION ? "https" : "http";
-const host = process.env.NEXT_PUBLIC_VERCEL_URL || "localhost:3000";
+const host = process.env.NEXT_PUBLIC_BASE_URL || "localhost:3000";
 
 export const config = {
-  baseUrl: `https://${process.env.NEXT_PUBLIC_VERCEL_URL}/api`,
+  baseUrl: `https://${process.env.NEXT_PUBLIC_BASE_URL}/api`,
 };
