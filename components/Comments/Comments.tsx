84b4a35454105df89efed4bf9bff811eec6f04ae
kelter-js <spawnkillerqw@gmail.com>
2024-05-14 21:56:58 +0500

    feat: implemented Comments component

---
+++ components/Comments/Comments.tsx
@@ -0,0 +1,28 @@
+import { FC } from "react";
+import { Comment as CommentType, EntityId } from "../../shared/types";
+import Comment from "../Comment/Comment";
+import { Container, List, Item } from "./CommentsStyle";
+import CommentForm from "../CommentForm";
+
+type CommentsProps = {
+  post: EntityId;
+  comments: CommentType[];
+};
+
+const Comments: FC<CommentsProps> = ({ post, comments }) => (
+  <Container id="comments">
+    <h3>Comments</h3>
+
+    <List>
+      {comments.map((comment) => (
+        <Item key={comment.id}>
+          <Comment comment={comment} />
+        </Item>
+      ))}
+    </List>
+
+    <CommentForm post={post} />
+  </Container>
+);
+
+export default Comments;
